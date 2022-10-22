d0bdd95ba68b625f0d91e9a8050c07784c848486
abed orabi <abedorabi@abeds-MacBook-Pro.local>
2022-10-22 19:44:07 +0200

    add seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,39 @@
+import { faker } from '@faker-js/faker';
 import { PrismaClient } from '@prisma/client';
-// const prisma = new PrismaClient();
-// async function main() {
-//   const user = prisma.user.upsert({
-//     where: { name: 'aa' },
-//     update: {},
-//     create: {
-//       name: 'abedaarabi',
-//     },
-//   });
-//   console.log({ user });
-// }
 
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
+const prisma = new PrismaClient();
+async function main() {
+  await prisma.user.deleteMany();
+  await prisma.material.deleteMany();
+  await prisma.project.deleteMany();
+
+  for (let i = 0; i < 100; i++) {
+    await prisma.user.create({
+      data: {
+        name: faker.company.name(),
+        materialName: {
+          create: {
+            materialName: faker.animal.fish(),
+            price: Number(faker.finance.amount(5, 10, 0)),
+            supplier: faker.address.cityName(),
+          },
+        },
+        projectName: {
+          create: {
+            projectName: faker.name.jobArea(),
+          },
+        },
+      },
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
